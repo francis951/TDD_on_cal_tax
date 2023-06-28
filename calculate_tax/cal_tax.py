520aff5db9fc470fe2a186438d6bd6a15c834d60
Francis Murungi <francis_951@live.com>
2023-06-29 02:27:38 +0300

    Begining my code

---
+++ calculate_tax/cal_tax.py
@@ -0,0 +1,10 @@
+# If you want a bit more practice at test first, write a tax calculator that calculates tax using the following brackets
+ 
+# If earnings < 12000 per year, pay no tax
+# then with earnings between 12,000 and 36,000 pay 20% tax
+# then with earnings greater than 36000 per year, pay 40% tax
+
+# Use a test first approach.  Commit at least after every passing test
+
+def cal_tax(earning):
+    pass
---
+++ calculate_tax/test_cal_tax.py
@@ -0,0 +1,6 @@
+"""
+"""
+from cal_tax import tax
+
+def test_cal_tax():
+    cal_tax(12000) == 12000
