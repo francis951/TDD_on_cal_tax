37566a9256d43094ea0e0e82ef407d665a09da1c
Francis Murungi <francis_951@live.com>
2023-06-30 08:41:33 +0300

    this is the second test that has t

--- calculate_tax/test_cal_tax.py
+++ calculate_tax/test_cal_tax.py
@@ -7,3 +7,6 @@ def test_cal_tax():
     """Testing for tax of those earning less than or equal 12000.00"""
     assert cal_tax(12000) == 00.0
 
+def test_cal_tax_1():
+    """Testing for tax of those earning more than or equal 36000.00"""
+    assert cal_tax(24000) == 2400.0
