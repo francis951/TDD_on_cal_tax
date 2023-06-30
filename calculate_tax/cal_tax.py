f4e403138913ef123714d002dad5b319a0c402d6
Francis Murungi <francis_951@live.com>
2023-06-30 08:37:42 +0300

    adding test

--- calculate_tax/cal_tax.py
+++ calculate_tax/cal_tax.py
@@ -4,8 +4,11 @@
 def cal_tax(earning):
     """The if conditions to calculate the paid tax :param: earning (int) which is then rounded"""
     if earning < 12000:
-        return earning
+        tax = 0
     if earning <= 36000:
-        return round((earning * 0.2), 2)
-    # else:
-    return round((earning * 0.4), 2)
+        tax = (earning - 12000) * 0.2
+    # if earning >= 36000:
+    else:
+        tax  = (earning - 36000) * 0.4
+    
+    return tax
